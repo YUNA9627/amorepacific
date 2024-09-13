72ead2498365c8723a0e5fb64b5066df9b3827b4
hanion77 <speciic77@naver.com>
2024-09-13 14:55:38 +0900

    Feat jquery로 수정

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-let mainBanner = document.querySelector(".main_banner");
-let video = mainBanner.querySelector(".video");
-let slides = video.querySelectorAll("li");
-let videos = video.querySelectorAll("video");
+let $mainBanner = $(".main_banner");
+let $video = $mainBanner.find(".video");
+let $slides = $video.find("li");
+let $videos = $video.find("video");
 let videoLength = [];
-let slideCount = slides.length;
+let slideCount = $slides.length;
 let currentSlideIdx = 0;
 let timer;
 
+console.log($videos);
 // 모든 비디오가 메타데이터 로드를 완료할 때까지 기다리기 위한 Promise 배열
 let metadataPromises = [];
 
-videos.forEach((item) => {
+$videos.each(function () {
+  let $this = $(this);
   // 각 비디오의 메타데이터가 로드될 때까지 기다리는 Promise 생성
   let metadataPromise = new Promise((resolve) => {
-    item.addEventListener("loadedmetadata", function () {
-      const duration = item.duration; // 재생 시간(초 단위)
+    $this.on("loadedmetadata", function () {
+      const duration = this.duration; // 재생 시간(초 단위)
       videoLength.push(duration);
       resolve(); // 메타데이터 로드 완료
     });
@@ -29,20 +31,25 @@ Promise.all(metadataPromises).then(() => {
 });
 
 if (slideCount > 1) {
-  slides.forEach((item, idx) => {
-    item.style.left = `${idx * 100}%`;
+  $slides.each(function (idx) {
+    $(this).css("left", `${idx * 100}%`);
   });
 }
 
 function moveSlide(num) {
-  video.style.left = `${-num * 100}%`;
+  $video.css("left", `${-num * 100}%`);
   currentSlideIdx = num;
   console.log(currentSlideIdx);
 
-  for (let sl of slides) {
-    sl.classList.remove("active");
-  }
-  slides[currentSlideIdx].classList.add("active");
+  $slides.removeClass("active").find('video').each(function () {
+    this.pause();
+    this.currentTime = 0;
+  });
+  
+  $slides.eq(currentSlideIdx).addClass("active").find('video').prop("muted", true).get(0).play().catch(error => {
+    console.error('Autoplay failed:', error);
+    // Handle the error (e.g., ask user to start playback manually)
+  });
 }
 moveSlide(0);
 
@@ -50,7 +57,6 @@ function autoSlide() {
   let time = videoLength[currentSlideIdx] * 1000;
   console.log(time);
   timer = setInterval(() => {
-    //let nextIdx = currentSlideIdx + 1;
     let nextIdx = (currentSlideIdx + 1) % slideCount;
     moveSlide(nextIdx);
   }, time);
