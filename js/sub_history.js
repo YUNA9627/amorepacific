44ff88141186a32e798f44704fa70472e6d8ac30
YUNA9627 <yuna2175@naver.com>
2024-09-24 18:19:14 +0900

    Feat sub_history js 작성

--- js/sub_history.js
+++ js/sub_history.js
@@ -4,28 +4,39 @@ const $historyBar = $('.history_bar');
 const $contList = $('.cont_list');
 const $items = $('.item');
 
-const pointerMaxScroll = $historyBar.height() - $pointer.height();
-const historyMaxScroll = $historyBar.height() - $historyBarOn.height();
-const listInnerHeight = $items.outerHeight();
-const contListOffsetTop = $contList.offset().top;
+let pointerMaxScroll = $historyBar.height() - $pointer.height();
+let historyMaxScroll = $historyBar.height() - $historyBarOn.height();
+let listInnerHeight = $items.outerHeight();
+let contListOffsetTop = $contList.offset().top;
 
 
-$(window).scroll(function(){
+function updateMeasurements() {
+  // 각 아이템의 높이 및 리스트 offset 값을 갱신
+  listInnerHeight = $items.outerHeight();
+  contListOffsetTop = $contList.offset().top;
+  pointerMaxScroll = $historyBar.height() - $pointer.height();
+  historyMaxScroll = $historyBar.height() - $historyBarOn.height();
+}
 
-  var scrollPos = $(window).scrollTop() + $(window).height() / 3;
 
+function history(){
+
+  var scrollPos = $(window).scrollTop() + $(window).height() / 2;
   $items.each(function(){
+
     var $this = $(this);
     var offsetTop = $this.offset().top;
     var offsetBottom = offsetTop + $this.outerHeight();
+    listInnerHeight = $this.outerHeight();
+
 
     if (scrollPos > offsetTop && scrollPos < offsetBottom) {
       let newTop = scrollPos - contListOffsetTop - listInnerHeight;
       let newHeight = scrollPos - contListOffsetTop - listInnerHeight;
-      
+     
       $this.addClass('active');
       $('.cont_nav').addClass('visible');
-    
+   
       $pointer.css('top', Math.max(newTop, 0) + 'px');
       $historyBarOn.css('height', Math.max(newHeight, 0) + 'px');
     } else {
@@ -33,47 +44,49 @@ $(window).scroll(function(){
         $('.cont_nav').removeClass('visible');
     }
 
+
     $('.cont_list ul').each(function(){
       var $this = $(this);
       var $child = $this.find('.item');
-      
+     
       if ($child.hasClass('active')) {
         $this.addClass('active');
       } else {
         $this.removeClass('active');
       }    
       });
-
-    // const scrollTop = $(window).scrollTop();
-  
-    // if (scrollTop > contListOffsetTop) {
-    //   const pointerNewTop = scrollTop - contListOffsetTop;
-    //   const historyNewTop = scrollTop - contListOffsetTop;
-      
-    //   $pointer.css('top', pointerNewTop + 'px');
-    //   $historyBarOn.css('height', historyNewTop + 'px');
-    // }else {
-    //   $pointer.css('top','0px');
-    //   $historyBarOn.css('height','0px');
-    // }
   });
+}
+
+
+// 리사이즈 시 높이 값과 offset 값을 다시 계산
+$(window).resize(function(){
+  updateMeasurements();
+  history();
 });
+
+
+// 스크롤 시에도 동작
+$(window).scroll(function(){
+  history();
+});
+
 
 $(window).scroll(function(){
   $contList.find('ul').each(function(index){
     if($(this).find('li:first-child').hasClass('active')){
-      $('.img_cont').eq(index).find('.false img').removeClass('show');
-      $('.img_cont').eq(index).find('.false img').addClass('false');
-      $('.img_cont').eq(index).find('.first img').removeClass('false');
-      $('.img_cont').eq(index).find('.first img').addClass('show');
-    }else if($(this).find('li:nth-child(3)').hasClass('active')){
-      $('.img_cont').eq(index).find('.first img').removeClass('show');
-      $('.img_cont').eq(index).find('.first img').addClass('false');
-      $('.img_cont').eq(index).find('.false img').removeClass('false');
-      $('.img_cont').eq(index).find('.false img').addClass('show');
+      // 첫 번째 아이템이 활성화된 경우
+      $('.img_cont').eq(index).siblings().find('.first img').removeClass('show');
+      $('.img_cont').eq(index).find('.false img').removeClass('show').addClass('false');
+      $('.img_cont').eq(index).find('.first img').removeClass('false').addClass('show');
+    } else if($(this).find('li:nth-child(3)').hasClass('active')){
+      // 세 번째 아이템이 활성화된 경우
+      $('.img_cont').eq(index).find('.first img').removeClass('show').addClass('false');
+      $('.img_cont').eq(index).find('.false img').removeClass('false').addClass('show');
     }
-  })
-})
+  });
+});
+
 
 $(window).scroll(function(){
   $('.cont_list ul').each(function(index) {
@@ -114,11 +127,20 @@ $(window).scroll(function(){
 });
 
 $(window).scroll(function(){
-  if($(this).scrollTop() > 600 && $(this).scrollTop() < 18000) {
+  let bottomHt = 280 + 110 + $('footer').height() + 'px';
+  let dcw = $(document).height() - $(window).height() - $('footer').height();
+  console.log(dcw);
+  if($(this).scrollTop() > 600 && $(this).scrollTop() < dcw) {
     $('.history_show').addClass('visible');
     $('.cont_nav').addClass('visible');
   } else {
     $('.history_show').removeClass('visible');
     $('.cont_nav').removeClass('visible');
+  }
+
+  if(dcw >= $(this).scrollTop()){
+    $('.history_show').css('bottom', bottomHt).css('top','');
+  }else{
+    $('.history_show').css('bottom', '0').css('top','0.5%');
   }
 })
