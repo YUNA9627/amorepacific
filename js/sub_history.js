7a6595a3aea8f228218e0d7a3f1fdb8a8dbcfa67
YUNA9627 <yuna2175@naver.com>
2024-09-26 11:06:31 +0900

    Feat sub_history js 작성

--- js/sub_history.js
+++ js/sub_history.js
@@ -178,19 +178,18 @@ $(window).scroll(function(){
 })
 
 
-  var lineprevST = 0;
 
+var lineprevST = 0;
   $(window).on('scroll', function() {
     var scrollTop = $(this).scrollTop();
     
-    console.log("현재 스크롤 위치: ", scrollTop);
-    console.log("이전 스크롤 위치: ", lineprevST);
-
     if (scrollTop > lineprevST) {
       $('.cont_nav').css('transform', 'translateY(0)');
     } else if (scrollTop < lineprevST) {
       $('.cont_nav').css('transform', 'translateY(54px)');
     }
+    console.log("현재 스크롤 위치: ", scrollTop);
+    console.log("이전 스크롤 위치: ", lineprevST);
 
     lineprevST = scrollTop;
   });
